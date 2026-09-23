efd8229c1743b633268bc1a4d7212710195f2877
231fa04843vu <231fa04843@gmail.com>
2026-09-23 22:21:26 +0530

    Fix Lottie rendering issue by switching to official react-lottie-player

--- tomoapp/src/components/PageLoader.jsx
+++ tomoapp/src/components/PageLoader.jsx
@@ -1,20 +1,18 @@
 import React from 'react';
-import { useLottie } from 'lottie-react';
+import { Player } from '@lottiefiles/react-lottie-player';
 import animationData from '../assets/delivery-rider.json';
 import './PageLoader.css';
 
 const PageLoader = () => {
-  const options = {
-    animationData: animationData,
-    loop: true,
-  };
-  
-  const { View } = useLottie(options);
-
   return (
     <div className="tomox-loader-wrapper">
-      <div className="lottie-container" style={{ width: 180, height: 180 }}>
-        {View}
+      <div className="lottie-container">
+        <Player
+          autoplay
+          loop
+          src={animationData}
+          style={{ height: '180px', width: '180px' }}
+        />
       </div>
       <h2 className="tomox-loader-text">
         <span className="bouncing-letters">
